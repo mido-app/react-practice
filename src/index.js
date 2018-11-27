12562531d06c36f7ce2ed8779f2aa2ff23d9bc29
Yutaka Omido <yutaka.omido@gmail.com>
2018-11-28 03:51:47 +0900

    add conditional rendering example

--- src/index.js
+++ src/index.js
@@ -126,6 +126,48 @@ class ClickCounter extends React.Component {
   }
 }
 
+// conditional component
+class Switch extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      active: false
+    }
+    this.switch = this.switch.bind(this)
+  }
+
+  render () {
+    let message
+    if (this.state.active) {
+      message = <p>ON</p>
+    } else {
+      message = <p>OFF</p>
+    }
+
+    return (
+      <div>
+        { message }
+        {
+          this.state.active &&
+          <p>This message is appear only when active state is true</p>
+        }
+        {
+          this.state.active ?
+          <p>This message is appear only when active state is true!!!</p> :
+          <p>This message is appear only when active state is false!!!</p>
+        }
+        <button onClick={this.switch}>Switch!</button>
+      </div>      
+    )
+  }
+
+  switch () {
+    this.setState(state => ({
+      active: !state.active
+    }))
+  }
+}
+
 // main components
 class App extends React.Component {
   render () {
@@ -149,6 +191,10 @@ class App extends React.Component {
         <div>
           <h2>Event Handling Samples</h2>
           <ClickCounter />
+        </div>
+        <div>
+          <h2>Conditional Rendering</h2>
+          <Switch />
         </div>
       </div> 
     )
