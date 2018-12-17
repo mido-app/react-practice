c01c1457ec4c449658333f677c792192aa780e20
Yutaka Omido <yutaka.omido@gmail.com>
2018-12-17 17:24:45 +0900

    add lifting state up sample

--- src/index.js
+++ src/index.js
@@ -318,7 +318,46 @@ class MultiForm extends React.Component {
   }
 }
 
-// main components
+// lifting state up component
+class PlusButton extends React.Component {
+  render () {
+    return (
+      <button onClick={() => this.props.onNumChange(this.props.value + this.props.step)}>
+        { this.props.value }+{ this.props.step }
+      </button>
+    )
+  }
+}
+
+// lifting state up receiver component
+class Counter extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      num: 0
+    }
+    this.onNumChange = this.onNumChange.bind(this)
+  }
+
+  onNumChange (newNum) {
+    this.setState((state, props) => ({
+      num: newNum
+    }))
+  }
+
+  render () {
+    return (
+      <div>
+        <div>current = { this.state.num }</div>
+        <PlusButton step={1} value={this.state.num} onNumChange={this.onNumChange} />
+        <PlusButton step={2} value={this.state.num} onNumChange={this.onNumChange} />
+        <PlusButton step={3} value={this.state.num} onNumChange={this.onNumChange} />
+      </div>
+    )
+  }
+}
+
+// main component
 class App extends React.Component {
   render () {
     const fruits = [
@@ -370,6 +409,10 @@ class App extends React.Component {
           <FormComponent label="Title" />
           <FruitDropdownComponent label="Favorite Fruits" fruits={fruits} />
           <MultiForm />
+        </div>
+        <div>
+          <h2>Lifting State Up Samples</h2>
+          <Counter />
         </div>
       </div> 
     )
